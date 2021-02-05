7512005e5710f1ecf7c2212566d9274b7b24dc22
QuangDoXuan <quangdo2206@gmail.com>
2021-02-05 14:02:52 +0700

    initial commit

---
+++ src/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/home/quang/Home/work/react_base/node_modules/umi/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','getInitialState','initialStateConfig','request',],
+});
+
+export { plugin };
