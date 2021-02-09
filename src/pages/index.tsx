ff9d5a8eabdf6aaaaa32299976cf545a69adfcdd
QuangDoXuan <quangdo2206@gmail.com>
2021-02-09 12:18:19 +0700

    react sample

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from "./index.less";
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>Welcome to My Page</h1>
+      <h1 className={styles.title}>Welcome to My Page React sample CICD</h1>
     </div>
   );
 }
