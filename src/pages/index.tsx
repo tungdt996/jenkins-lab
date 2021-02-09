4607be9fc1f0c4b7e44a0601422dc866035a54b5
Quang <quang.do@amela.vn>
2021-02-09 12:04:25 +0700

    update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import styles from './index.less';
+import styles from "./index.less";
 
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title}>Welcome to My Page</h1>
     </div>
   );
 }
