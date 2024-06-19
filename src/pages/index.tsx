73d1ef8200aa31a38aa57adb05c57e8bbd4d9a08
tungdt996 <tungdt996@gmail.com>
2024-06-19 10:18:28 +0700

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from "./index.less";
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>Welcome to My Page React sample CICD by Tungdt</h1>
+      <h1 className={styles.title}>Welcome to My Page React sample CICD by Tungdt ver1</h1>
     </div>
   );
 }
